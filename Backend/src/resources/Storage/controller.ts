c34b345e84ec44e1f2ec816a8517d7850db5f1c1
yidye <yidnekachewtebeje@gmail.com>
2022-10-08 20:55:24 +0300

    update storage controller

--- Backend/src/resources/Storage/controller.ts
+++ Backend/src/resources/Storage/controller.ts
@@ -1,20 +1,32 @@
 import { Request } from "express";
-import StorageService from "../../services/StorageService";
 import busboy from 'busboy'
 import { randomFillSync } from "node:crypto";
 import { Connection } from "mongoose";
+import { createWriteStream, rm } from "node:fs";
+import path from "node:path";
+import * as os from "os";
+import FileRepository from '../../Repository/FileRepository';
+import { getStorage } from "firebase-admin/storage";
 
 class StorageController {
-    storageService;
+    fileRepository;
+    cloudStorage;
     constructor() {
-        this.storageService = new StorageService;
+        this.fileRepository = new FileRepository();
+        this.cloudStorage = getStorage().bucket();
+    }
+    random() {
+        const buf = Buffer.alloc(16);
+        return () => randomFillSync(buf).toString('hex');
     }
    
     async upload(req: Request, res: Response) {
         const bb = busboy({ headers: req.headers });
+        const saveto = path.join(os.tmpdir(), `busboy-upload-${this.random()}`);
         bb.on('file', (name, file, info) => {
             const { filename, encoding, mimeType } = info;
-            this.storageService.upload(file, filename);
+            const f = createWriteStream(saveto);
+            file.pipe(f);
         });
         bb.on('field', (name, val, info) => {
             console.log(`Field [${name}]: value: %j`, val);
@@ -24,7 +36,15 @@ class StorageController {
             res.headers.set("Connection", 'close');
             res.headers.set("Location", '/');
         });
-        req.pipe(bb); 
+        req.pipe(bb);
+        this.cloudStorage.upload(saveto).then(data => {
+            console.log('upload success', data);
+        }).catch(err => {
+            console.log('error uploading to storage', err);
+        });
+        rm(saveto, (err) => {
+            console.log(err);
+        });
         return;
     }
 }
